daf64d39886db206924488151c52179610ba1eff
kevin <kevinrangels>
2021-02-13 17:59:12 -0500

    finish section

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,27 +1,45 @@
 import React from 'react'
+import moment from 'moment'
+import { useDispatch } from 'react-redux'
+import { activeNote } from '../../actions/notes'
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, title, body, url}) => {
+
+    const noteDate = moment(date)
+    const dispatch = useDispatch()
+
+    const handleEntryClick = () => {
+        dispatch(
+            activeNote(id, {
+                date, title, body, url
+            }))
+    }
     return (
-        <div className="journal__entry pointer">
-            <div
-              className="journal__entry-picture"
-              style={{
-                  backgroundSize: 'cover',
-                  backgroundImage: 'url(https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2020/09/dragon-ball-kame-house-mutenroshi-existe-mundo-real-2061839.jpg)'
-              }}
-            >
-            </div>
+        <div
+          className="journal__entry pointer animate__animated animate__fadeIn animate__faster"
+          onClick={handleEntryClick}
+        >
+            {
+                url &&
+                <div
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: `url(${url})`
+                }}
+                ></div>
+            }
             <div className="journal__entry-body">
                 <p className="journal__entry-title">
-                    Un nuevo dia
+                    {title}
                 </p>
                 <p className="journal__entry-p">
-                  texto  textotexto texto vtextotextotextotextotexto textoasd
+                  {body}
                 </p>
             </div>
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+            <span>{noteDate.format('dddd')}</span>
+                <h4>{noteDate.format('Do')}</h4>
             </div>
         </div>
     )
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote, startDeleting } from '../../actions/notes'
+import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const { active:note } = useSelector(state => state.notes)
+    const [formValues, handleInputChange, reset] = useForm(note)
+    const {body, title, id} = formValues
+    const dispatch = useDispatch()
+
+
+    const activeId = useRef(note.id)
+
+    useEffect(() => {
+      if (note.id !== activeId.current) {
+        reset(note)
+        activeId.current = note.id
+      }
+    }, [note, reset])
+
+    useEffect(() => {
+      dispatch( activeNote(formValues.id, {...formValues}) )
+    }, [formValues, dispatch])
+
+    const handleDeleteNote = () => {
+      dispatch( startDeleting(id) )
+    }
+    
+
     return (
         <div className="notes__main-content">
             <NotesAppBar/>
@@ -12,19 +40,36 @@ export const NoteScreen = () => {
                       placeholder="Some awesome title"
                       className="notes__title-input"
                       autoComplete="off"
+                      name="title"
+                      value={title}
+                      onChange={handleInputChange}
                     />
                     <textarea
                       placeholder="What happened input"
                       className="notes__textarea"
+                      value={body}
+                      name="body"
+                      onChange={handleInputChange}
                     ></textarea>
-                    <div className="notes__image">
-                        <img
-                          src="https://ih1.redbubble.net/image.937839891.0711/flat,750x1000,075,f.jpg"
-                          alt="imagen"
-                        />
-                    </div>
+                    {
+                      (note.url) &&
+                      (
+                        <div className="notes__image">
+                          <img
+                            src={note.url}
+                            alt="imagen"
+                          />
+                        </div>
+                      )
+                    }
                 {/* </form> */}
             </div>
+            <button
+              className="btn btn-danger"
+              onClick={handleDeleteNote}
+            >
+              Delete
+            </button>
         </div>
     )
 }
